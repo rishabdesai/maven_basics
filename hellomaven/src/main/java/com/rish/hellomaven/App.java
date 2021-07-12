8cb561fded4bb434de0bf5f6d6c84b37116b5ac6
desai.rishab <desai.rishab@gmail.com>
2021-07-12 18:42:05 +0530

    hellomaven project added

---
+++ hellomaven/src/main/java/com/rish/hellomaven/App.java
@@ -0,0 +1,13 @@
+package com.rish.hellomaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
